7718bb498805770ca249e181a6a30ae34c561d28
Arjun <arjuni5acer12@gmail.com>
2026-02-22 16:44:04 +0530

    Fix authentication issue by allowing cross-site cookies

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,16 +1,18 @@
-export const sendToken=(user,statusCode,res)=>{
-    const token=user.getJWTToken();
+export const sendToken = (user, statusCode, res) => {
+    const token = user.getJWTToken();
 
     // options for cookies
-    const options={
-        expires:new Date(Date.now()+process.env.EXPIRE_COOKIE*24*60*60*1000),
-        httpOnly:true
+    const options = {
+        expires: new Date(Date.now() + process.env.EXPIRE_COOKIE * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
     }
     res.status(statusCode)
-    .cookie('token',token,options)
-    .json({
-        success:true,
-        user,
-        token
-    })
+        .cookie('token', token, options)
+        .json({
+            success: true,
+            user,
+            token
+        })
 }
